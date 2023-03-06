024dc383a717257d2bfc8d1e2cb83e92ce4413d5
Óscar <oscarlm27@gmail.com>
2023-03-06 19:57:25 +0100

    mostrar users

--- resources/js/usuarios.js
+++ resources/js/usuarios.js
@@ -1,84 +1,22 @@
 
-const tbody = document.querySelector('#usuarios');
-
-fetch('mostrar')
-  .then(response => response.json())
-  .then(data => {
-    data.usuarios.forEach(usuario => {
-      const tr = document.createElement('tr');
-      tr.innerHTML = `
-        <td>${usuario.id}</td>
-        <td>${usuario.nombre}</td>
-        <td>${usuario.email}</td>
-        <td>${usuario.password}</td>
-        <td>${usuario.admin}</td>
-      `;
-      tbody.appendChild(tr);
+function getUsers() {
+fetch('usuarios')
+.then(response => response.json())
+.then(data => {
+    let usuarios = document.getElementById('usuarios');
+    data.forEach(product => {
+        let tr = document.createElement('tr');
+        tr.innerHTML = `
+            <td>${product.id}</td>
+            <td>${product.nombre}</td>
+            <td>${product.email}</td>
+            <td>${product.password} €</td>
+            <td>${product.admin}</td>
+            <td><button onclick="mod(${product.id})">Modificar</button><td>
+            <td><button onclick="el(${product.id})">Eliminar</button><td>
+        `;
+        usuarios.appendChild(tr);
     });
-  })
-  .catch(error => console.error(error));
-
-// function getUsuarios() {
-// fetch('mostrar')
-// .then(response => response.json())
-// .then(data => {
-//     let usuarios = document.getElementById('usuarios');
-//     data.forEach(usuario => {
-//         let table = document.createElement('tr');
-//         table.innerHTML = `
-//             <td>${usuario.id}</td>
-//             <td>${usuario.nombre}</td>
-//             <td>${usuario.email}</td>
-//             <td>${usuario.password} €</td>
-//             <td>${usuario.admin}</td>
-//             <button onclick="mod(${usuario.id})">Modificar</button>
-//             <button onclick="el(${usuario.id})">Eliminar</button>
-
-//         `;
-//         usuarios.appendChild(table);
-//     });
-// });
-// }
-// getUsuarios();
-
-function mostrarUsuarios() {
-  var request = new XMLHttpRequest();
-  request.open('GET', 'usuarios', true);
-  request.responseType = 'json';
-
-  request.onload = function() {
-      if (request.status === 200) {
-          var usuarios = request.response;
-          mostrarListaUsuarios(usuarios);
-      } else {
-          console.log('Error al cargar los usuarios');
-      }
-  }
-
-  request.send();
+});
 }
-
-function mostrarListaUsuarios(usuarios) {
-  var listaUsuarios = document.getElementById('usuarios');
-
-  listaUsuarios.innerHTML = '';
-
-  usuarios.forEach(function(usuario) {
-      var tr = document.createElement('tr');
-      var tdNombre = document.createElement('td');
-      tdNombre.textContent = usuario.nombre;
-      tr.appendChild(tdNombre);
-      var tdEmail = document.createElement('td');
-      tdEmail.textContent = usuario.email;
-      tr.appendChild(tdEmail);
-      var tdPassword = document.createElement('td');
-      tdPassword.textContent = usuario.password;
-      tr.appendChild(tdPassword);
-      var tdAdmin = document.createElement('td');
-      tdAdmin.textContent = usuario.admin;
-      tr.appendChild(tdAdmin);
-      listaUsuarios.appendChild(tr);
-  });
-}
-
-mostrarUsuarios();
+getUsers();
--- resources/views/usuarios.blade.php
+++ resources/views/usuarios.blade.php
@@ -24,29 +24,8 @@
 
         </tbody>
     </table>
-    <!-- <script src="usuarios.js"></script> -->
-    <script type="text/javascript">
-                function getUsers() {
-            fetch('usuarios')
-                .then(response => response.json())
-                .then(data => {
-                    let usuarios = document.getElementById('usuarios');
-                    data.forEach(product => {
-                        let tr = document.createElement('tr');
-                        tr.innerHTML = `
-                            <td>${product.id}</td>
-                            <td>${product.nombre}</td>
-                            <td>${product.email}</td>
-                            <td>${product.password} €</td>
-                            <td>${product.admin}</td>
-                            <td><button onclick="mod(${product.id})">Modificar</button><td>
-                            <td><button onclick="el(${product.id})">Eliminar</button><td>
-                        `;
-                        usuarios.appendChild(tr);
-                    });
-                });
-        }
-        getUsers();
+    <script src="../resources/js/usuarios.js"></script>
+
     </script>
 </body>
 </html>
